79788081872274971f35e6f4b8c737e43383bba2
Bodil Stokke <bodil@bodil.tv>
2011-09-16 20:37:02 +0200

    Problem 5.

---
+++ src/euler/p5.clj
@@ -0,0 +1,29 @@
+(ns euler.p5
+  (:require [clojure [test :as test]]))
+
+(test/with-test
+  (defn divisible-by? [n ds]
+    "True if n is divisible by all numbers in ds."
+    (every? #(= 0 (rem n %1)) ds))
+
+  (test/is (divisible-by? 6 [2 3]))
+  (test/is (divisible-by? 15 [3 5]))
+  (test/is (not (divisible-by? 15 [3 5 10]))))
+
+(test/with-test
+  (defn first-divisible-by [ns ds]
+    "Find the first number in ns divisible by all numbers in ds."
+    (if (seq ns)
+      (let [n (first ns)]
+        (if (divisible-by? n ds) n
+            (recur (rest ns) ds)))
+      nil))
+
+  (test/is (= (first-divisible-by (range 1 10) [2 3])) 6)
+  (test/is (= (first-divisible-by (range 2 5000 2) (range 2 11)) 2520)))
+
+;; Save search time by only evaluating increments of 20, because we
+;; know the answer must be divisible by 20.
+(defn euler-p5 []
+  (first-divisible-by (range 20 Double/POSITIVE_INFINITY 20) (range 2 21)))
+
