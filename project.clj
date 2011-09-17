600331053c527372e3aeb164cc94643006d5b44e
Bodil Stokke <bodil@bodil.tv>
2011-09-17 14:25:52 +0200

    Literate programming is go!

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :description "Project Euler in Clojure"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[marginalia "0.5.0"]])
